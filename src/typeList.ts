9c76f4aa116bd5897544d6dea115896e8b06b5a9
Takahisa Hirano <54578410+Hiratto@users.noreply.github.com>
2022-03-15 21:31:21 +0900

    TypeScript基本の型

--- src/typeList.ts
+++ src/typeList.ts
@@ -1,1 +1,42 @@
-a;
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+// TypeScript基本の型
+
+// boolean
+
+let bool: boolean = true;
+
+// number 数値
+
+let num: number = 0;
+
+// string　文字
+
+let str: string = "A";
+
+// Array　配列
+
+let arr1: Array<number> = [0,1,2];
+let arr2: number[] = [0,1,2];
+
+// tuple 二つの要素を指定できる
+
+let tuple: [number,string] = [0,"A"];
+
+// any なるべく使わないように
+
+let any1: any = false;
+
+// void（何も返却していない状態）
+
+const fancA = ():void => {
+  const test = "TEST";
+}
+
+// null
+
+let null: null = null;
+
+// undefined
+
+let undefined1: undefined = undefined;
